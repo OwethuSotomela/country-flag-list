6878eeda5c406a1360e64cfb2da5263df87ea0bf
OwethuSotomela <owethusotomela@gmail.com>
2022-02-28 12:05:53 +0200

    Displaying contries with flags

--- js/app.js
+++ js/app.js
@@ -1,7 +1,92 @@
-var countryName = document.querySelector(".nameOfCountry");
+var countryName = document.querySelector(".enterCountry");
 var addBtn = document.querySelector(".addBtn");
-
-
+var display = document.querySelector(".myList");
 
 var templateSource = document.querySelector(".CFLTemplate").innerHTML;
 var CFLTemplate = Handlebars.compile(templateSource);
+
+// const countries = ["Argentina", "Brazil", "Chile", "Zambia", "Uganda", "Malawi", "Rwanda", "Ireland", "Switzerland"];
+// const flags = ["🇦🇷", "🇧🇷", "🇨🇱", "🇿🇲", "🇺🇬", "🇲🇼", "🇷🇼", "🇮🇪", "🇨🇭"];
+var nameOf;
+
+const flagList = [
+    {
+        country: "Argentina",
+        flag: "🇦🇷",
+    },
+    {
+        country: "Brazil",
+        flag: "🇧🇷",
+    },
+    {
+        country: "Chile",
+        flag: "🇨🇱",
+    },
+    {
+        country: "Zambia",
+        flag: "🇿🇲",
+    },
+    {
+        country: "Uganda",
+        flag: "🇺🇬",
+    },
+    {
+        country: "Malawi",
+        flag:  "🇲🇼",
+    },
+    {
+        country: "Rwanda",
+        flag:  "🇷🇼",
+    },
+    {
+        country: "Ireland",
+        flag: "🇮🇪",
+    },
+    {
+        country: "Switzerland",
+        flag: "🇨🇭",
+    }
+]
+
+
+function displayElems() {
+    let element = document.querySelector(".myList")
+    var node = document.createElement("li");
+    var textnode = document.createTextNode(flagList)
+    node.appendChild(textnode);
+    if (element != null) {
+        element.appendChild(node);
+    }
+
+    display.innerHTML = CFLTemplate({
+        list: flagList, get country() {
+            return this.country;
+        },
+        set country(value) {
+            this.country = value;
+        },
+        list: flagList, get flag() {
+            return this.flag;
+        },
+        set flag(value) {
+            this.flag = value;
+        },
+    }
+    )
+
+    console.log(flagList)
+    return flagList;
+}
+
+
+function addFlag() {
+    if (countryName.value != null) {
+        nameOf = document.querySelector(".enterCountry").value;
+    }
+
+    return displayElems();
+
+}
+
+
+addBtn.addEventListener("click", addFlag)
--- js/country-FL-widget.js
+++ js/country-FL-widget.js
@@ -1,6 +1,25 @@
-function CountryFlagList() {
+function CountryFlagList(local) {
 
     const countries = ["Argentina", "Brazil", "Chile", "Zambia", "Uganda", "Malawi", "Rwanda", "Ireland", "Switzerland"];
     const flags = ["🇦🇷", "🇧🇷", "🇨🇱", "🇿🇲", "🇺🇬", "🇲🇼", "🇷🇼", "🇮🇪", "🇨🇭"];
+    var countryList = local;
+
+    function addCountry(c_name){
+        console.log(c_name)
+        if (!countryList.includes(c_name)){
+            countryList.push(c_name)
+        }
+        return c_name;
+    }
+
+    function getCountry(){
+        console.log(countryList)
+        return countryList;
+    }
+
+    return {
+        addCountry,
+        getCountry
+    }
 
 }

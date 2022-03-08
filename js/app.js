151c953de76032fc2404204c4d4c4b9393d2c30a
OwethuSotomela <owethusotomela@gmail.com>
2022-03-08 09:44:33 +0200

    Let replacing var

--- js/app.js
+++ js/app.js
@@ -1,23 +1,21 @@
-var countryName = document.querySelector(".enterCountry");
-var countryFlag = document.querySelector(".enterFlag");
-var addBtn = document.querySelector(".addBtn");
-var resetBtn = document.querySelector(".resetBtn");
-var display = document.querySelector(".myList");
-var feedback = document.querySelector(".feedback");
-var sortAscending = document.querySelector(".ascending");
-var sortDescending = document.querySelector(".descending");
-var search = document.querySelector(".search");
+let countryName = document.querySelector(".enterCountry");
+let countryFlag = document.querySelector(".enterFlag");
+let addBtn = document.querySelector(".addBtn");
+let resetBtn = document.querySelector(".resetBtn");
+let display = document.querySelector(".myList");
+let feedback = document.querySelector(".feedback");
+let sortAscending = document.querySelector(".ascending");
+let sortDescending = document.querySelector(".descending");
+let search = document.querySelector(".search");
 
-var templateSource = document.querySelector(".CFLTemplate").innerHTML;
-var CFLTemplate = Handlebars.compile(templateSource);
+let templateSource = document.querySelector(".CFLTemplate").innerHTML;
+let CFLTemplate = Handlebars.compile(templateSource);
 
-// const countries = ["Argentina", "Brazil", "Chile", "Zambia", "Uganda", "Malawi", "Rwanda", "Ireland", "Switzerland"];
-// const flags = ["🇦🇷", "🇧🇷", "🇨🇱", "🇿🇲", "🇺🇬", "🇲🇼", "🇷🇼", "🇮🇪", "🇨🇭"];
-var nameOf = countryName.value;
-var flagOf = countryFlag.value;
+let nameOf = countryName.value;
+let flagOf = countryFlag.value;
 message = '';
 
-var flagList = [
+let flagList = [
     {
         country: "Argentina",
         flag: "🇦🇷",
@@ -55,37 +53,22 @@ var flagList = [
         flag: "🇨🇭",
     }
 ]
-flagList = flagList;
 
+flagList = flagList;
 if (localStorage[{ flagOf, nameOf }]) {
-    flagList = JSON.parse(localStorage.getItem({ flagOf, nameOf }));
+    flagList = JSON.parse(localStorage.getItem('Flags + Countries'));
 }
 
 const capitals = CountryFlagList(flagList);
 
 let element = document.querySelector(".myList")
-var node = document.createElement("li");
-var textnode = document.createTextNode(flagList)
+let node = document.createElement("li");
+let textnode = document.createTextNode(flagList)
 node.appendChild(textnode);
-
-// display.innerHTML = CFLTemplate({
-//     list: flagList, get country() {
-//         return this.country;
-//     },
-//     set country(value) {
-//         this.country = value;
-//     },
-//     list: flagList, get flag() {
-//         return this.flag;
-//     },
-//     set flag(value) {
-//         this.flag = value;
-//     },
-// })
 
 function addCountryAndFlag() {
 
-    var regexFlag = /[\uD83C][\uDDE6-\uDDFF][\uD83C][\uDDE6-\uDDFF]/.test(flagOf);
+    let regexFlag = /[\uD83C][\uDDE6-\uDDFF][\uD83C][\uDDE6-\uDDFF]/.test(flagOf);
 
     if (regexFlag != null) {
         regexFlag = countryFlag.value;
@@ -128,25 +111,10 @@ function reset() {
     capitals.clearStorage()
 }
 
-function descending() {
-    // display.innerHTML = JSON.stringify(flagList.reverse());
-    display.innerHTML = CFLTemplate({
-        list: flagList, get country() {
-            return this.country;
-        },
-        set country(value) {
-            this.country = value;
-        },
-        list: flagList, get flag() {
-            return this.flag;
-        },
-        set flag(value) {
-            this.flag = value;
-        },
-    })
+function ascending() {
+
 }
 
 addBtn.addEventListener("click", addFlag);
 resetBtn.addEventListener("click", reset);
-sortAscending.addEventListener("click", ascending);
-sortDescending.addEventListener("click", descending);
+sortAscending.addEventListener("clink", ascending)
